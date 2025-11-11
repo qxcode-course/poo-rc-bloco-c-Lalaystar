02dccb5ce1a41067e8fb48192b971c0bfa7ed6a8
Layanne <layannemenezes78@gmail.com>
2025-11-11 09:55:01 -0300

    entrar

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,8 +1,36 @@
-class Pessoa:
+class Person:
     def __init__(self, nome:str):
         self.nome=nome
     def __str__(self):
         return self.nome
 
-class Onibus:
-    def __init__(self):
+class Mark:
+    def __init__(self,n_caixas:int):
+        self.espera:list[Person]=[]
+        self.caixas:list[Person|None]=[]
+        for _ in range(n_caixas):
+            self.caixas.append(None)
+
+    def arrive(self, pessoa:Person):
+        self.espera.append(pessoa)
+
+    def __str__(self):
+        cadeiras=", ".join([str(x) if x else "-----" for x in self.caixas])
+        espera=", ".join([str(x) for x in self.espera])
+        return f"Caixas: [{cadeiras}]\nEspera: [{espera}]"
+
+def main():
+    mark=Mark(2)
+    while True:
+        line=input()
+        print("$"+line)
+        args=line.split()
+        
+        if args[0]=="end":
+            break
+        if args[0]=="show":
+            print (mark)
+        if args[0]=="arrive":
+            mark.arrive(str(args[1]))
+    
+main()
