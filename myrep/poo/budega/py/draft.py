251d4dacb7007661c60e840d8c609c7cabc47bc7
Layanne <layannemenezes78@gmail.com>
2025-11-12 11:42:36 -0300

    budegafeita

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -14,6 +14,32 @@ class Mark:
     def arrive(self, pessoa:Person):
         self.espera.append(pessoa)
 
+    def call(self,index:int):
+        if index<0 or index>=len(self.caixas):
+            print("fail: index invalido")
+            return
+        if self.caixas[index] is not None:
+            print("fail: caixa ocupado") 
+            return
+        if len(self.espera)==0:
+            print("fail: sem clientes")
+            return
+    
+        self.caixas[index]=self.espera[0]
+        del self.espera[0]
+    
+    def finish(self,index:int)->Person|None:
+        if index<0 or index>=len(self.caixas):
+            print("fail: caixa inexistente")
+            return None
+        if self.caixas[index] is None:
+            print("fail: caixa vazio")
+            return None
+        
+        aux=self.caixas[index]
+        self.caixas[index]=None
+        return aux
+
     def __str__(self):
         cadeiras=", ".join([str(x) if x else "-----" for x in self.caixas])
         espera=", ".join([str(x) for x in self.espera])
@@ -28,9 +54,15 @@ def main():
         
         if args[0]=="end":
             break
-        if args[0]=="show":
+        elif args[0]=="show":
             print (mark)
-        if args[0]=="arrive":
+        elif args[0]=="init":
+            mark=Mark(int(args[1]))
+        elif args[0]=="arrive":
             mark.arrive(str(args[1]))
+        elif args[0]=="call":
+            mark.call(int(args[1]))
+        elif args[0]=="finish":
+            mark.finish(int(args[1]))
     
 main()
