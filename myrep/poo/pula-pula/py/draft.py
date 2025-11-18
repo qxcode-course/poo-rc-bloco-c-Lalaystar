497ada954844929fb57ae985bb0bbbe94b2e8425
Layanne <layannemenezes78@gmail.com>
2025-11-18 20:04:19 -0300

    aaaaaaaaa

---
+++ myrep/poo/pula-pula/py/draft.py
@@ -0,0 +1,22 @@
+class Kid:
+    def __init__(self,age:int,name:str):
+        self.__age=age
+        self.__name=name
+    
+    def getAge(self):
+        return self.__age
+    
+    def getName(self):
+        return self.__name
+    
+    def setName()
+        
+    def main():
+        
+
+
+
+
+
+
+main()
