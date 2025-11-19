ee651eb0e2ffdfb7fa470e78f21c72de70d6abbd
Layanne <layannemenezes78@gmail.com>
2025-11-18 21:02:12 -0300

    amoarrays

--- myrep/poo/array/py/draft.py
+++ myrep/poo/array/py/draft.py
@@ -5,7 +5,34 @@ class Foo:
 frutas:list[str]=["banana", "uva"]
 coisas:list[Foo]=[Foo("Maria"), Foo("Joana")]
 
-p=input()
-for x in frutas:
-    frutas.append(p)
-    print(x)
+frutas.append("maça")
+frutas.pop(1)
+
+frutas.insert(1,"pera")
+
+#n=int(input())
+#bixa=list(range(n+1))
+
+tam=len(frutas)
+
+#for x in range(n):
+  #  print(x)
+
+bixa2:list[int]=[1,2,3,4,5,6,7,8,9,10]
+#print(5 in bixa2)
+#x=9
+###
+# encontrado = False
+#for elemento in bixa2:
+ #   if elemento == x:
+  #      encontrado = True
+   #     break
+    #print(encontrado)
+#par=[x for x in bixa2 if x%2==0]
+#quadrado=[x*x for x in bixa2]
+#print(quadrado)
+
+x=7
+for x in bixa2:
+    bixa2.remove(x)
+print(bixa2)
