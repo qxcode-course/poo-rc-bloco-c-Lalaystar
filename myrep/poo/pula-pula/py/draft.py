81e610f79a7d0772bd45ee1e41e8b4cfe3a230d2
Layanne <layannemenezes78@gmail.com>
2025-11-18 22:15:38 -0300

    minhasaidanaosai

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,22 +1,52 @@
 class Kid:
-    def __init__(self,age:int,name:str):
+    def __init__(self,name:str, age:int):
         self.__age=age
         self.__name=name
-    
-    def getAge(self):
-        return self.__age
-    
-    def getName(self):
+
+    def getName(self)->str:
         return self.__name
     
-    def setName()
+    def getAge(self)->int:
+        return self.__age
+    
+    def setName(self, name:str):
+        self.__name=name
+
+    def setAge(self, age:int):
+        self.__age=age
+
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self):
+        return f"{self.__name}:{self.__age}"
+    
+class Pula:
+    def __init__(self):
+        self.espera:list[Kid]=[]
+        self.la:list[Kid]=[]
         
-    def main():
-        
+    def arrive(self, name: str, age: int):
+        self.espera.append(Kid(name, age))
 
+    def __str__(self):
+        return f"{self.espera} => {self.la}"
 
+def main():
+    pula=Pula()
+    while True:
+        line=input()
+        print("$"+line)
+        args=line.split()
 
-
-
-
+        if args[0]=="end":
+            break
+        if args[0]=="show":
+            print(pula)
+        if args[0]=="arrive":
+            name=args[1]
+            age=int(args[2])
+            pula.arrive(name,age)
 main()
+        
+   
